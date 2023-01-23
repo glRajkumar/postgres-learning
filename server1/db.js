2b005f106eb337066ba20aedffd85d54215afcf4
Raj kumar <glrajv369@gmail.com>
2023-01-23 10:45:31 +0530

    db connected

---
+++ server1/db.js
@@ -0,0 +1,24 @@
+const { Pool } = require("pg")
+
+// we can connect to db by simply using the following method
+// const pool = new Pool()
+
+// connection variables will automatically taken from .env file if we set variables as follows
+// PGUSER=
+// PGHOST=
+// PGPASSWORD=
+// PGDATABASE=
+// PGPORT=
+
+// since i am using cloud based db, i am connected using connection string method (i tried ssl approach but not worked as expected. since for learning purpose i dont want to spend much time)
+const pool = new Pool({
+  connectionString: process.env.connectionString
+})
+
+pool.connect()
+  .then(c => console.log("connected db"))
+  .catch(e => console.log("err", e))
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+}
--- server1/index.js
+++ server1/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
 
 const app = express()
+require('./db')
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
