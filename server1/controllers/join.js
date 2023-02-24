f5d91bf38a6b59f95907991104ef00b0f526aad5
Raj kumar <glrajv369@gmail.com>
2023-02-24 10:52:08 +0530

    left and right joins

--- server1/controllers/join.js
+++ server1/controllers/join.js
@@ -16,4 +16,28 @@ router.get("/inner", async (req, res) => {
   }
 })
 
+router.get("/left", async (req, res) => {
+  try {
+    const qTxt = "SELECT e.name AS emp_name, d.name AS dept_name FROM employees e LEFT JOIN departments d ON e.dept_id = d.id"
+
+    const { rows } = await query(qTxt)
+    res.send(rows)
+
+  } catch (err) {
+    res.status(400).json({ err, msg: "" })
+  }
+})
+
+router.get("/right", async (req, res) => {
+  try {
+    const qTxt = "SELECT e.name AS emp_name, d.name AS dept_name FROM employees e RIGHT JOIN departments d ON e.dept_id = d.id"
+
+    const { rows } = await query(qTxt)
+    res.send(rows)
+
+  } catch (err) {
+    res.status(400).json({ err, msg: "" })
+  }
+})
+
 module.exports = router
